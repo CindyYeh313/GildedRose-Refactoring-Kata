7463ddc95f65c65169aad207ba7e4fdc0fc584e4
cindy.yeh <cindy.yeh@104.com.tw>
2022-04-07 17:37:53 +0800

    物品功能拆分

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -6,10 +6,65 @@ class Item {
     this.limit = 50
     this.lowerLimit = 0
   }
-  updateQualityValue(value){
+
+  updateQualityValue (value){
     if( this.quality < this.limit && this.quality > this.lowerLimit ) this.quality += value
   }
+
+  updateDaily(){
+    switch(this.name){
+      case 'Sulfuras, Hand of Ragnaros':
+        this.updateSulfuras()
+        break
+      case 'Aged Brie':
+        this.updateAgedBrie()
+        break
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        this.updateBackstage()
+        break
+      default:
+        this.updateNormal()
+    }
+  }
+
+  updateSulfuras(){
+    return this
+  }
+
+  updateAgedBrie(){
+    this.updateQualityValue(1)
+    this.sellIn --
+    if (this.sellIn < 0) {
+      this.updateQualityValue(1)
+    }  
+    return this
+  }
+
+  updateBackstage(){
+    this.updateQualityValue(1)
+    if (this.sellIn < 11) {
+      this.updateQualityValue(1)
+    }
+    if (this.sellIn < 6) {
+      this.updateQualityValue(1)
+    }
+    this.sellIn --
+    if (this.sellIn < 0) {
+      this.updateQualityValue(-this.quality)
+    }
+    return this
+  }
+
+  updateNormal(){
+    this.updateQualityValue(-1)
+    this.sellIn --
+    if (this.sellIn < 0) {
+      this.updateQualityValue(-1)
+    }
+    return this
+  }
 }
+
 
 class Shop {
   constructor(items=[]){
@@ -18,40 +73,7 @@ class Shop {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if(this.items[i].name == 'Sulfuras, Hand of Ragnaros') return this.items
-
-      if (this.items[i].name == 'Aged Brie') {
-        this.items[i].updateQualityValue(1)
-      }
-      else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-        this.items[i].updateQualityValue(1)
-        if (this.items[i].sellIn < 11) {
-          this.items[i].updateQualityValue(1)
-        }
-        if (this.items[i].sellIn < 6) {
-          this.items[i].updateQualityValue(1)
-        }
-      } else {
-        this.items[i].updateQualityValue(-1)
-      }
-
-      this.items[i].sellIn = this.items[i].sellIn - 1;
-      
-      if (this.items[i].name == 'Aged Brie'){
-        if (this.items[i].sellIn < 0) {
-          this.items[i].updateQualityValue(1)
-        }  
-      }
-      else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
-        if (this.items[i].sellIn < 0) {
-          this.items[i].updateQualityValue(-this.items[i].quality)
-        }
-      } 
-      else {
-        if (this.items[i].sellIn < 0) {
-          this.items[i].updateQualityValue(-1)
-        }
-      }
+      this.items[i].updateDaily()
     }
     return this.items;
   }
