e941954039dba2174d1e7a39cc092711110d989e
cindy.yeh <cindy.yeh@104.com.tw>
2022-04-07 15:44:39 +0800

    銷售期限判斷式搬移

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -40,19 +40,22 @@ class Shop {
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name == 'Aged Brie'){
+      if (this.items[i].name == 'Aged Brie'){
+        if (this.items[i].sellIn < 0) {
           this.items[i].quality = this.increaseQuality(this.items[i].quality)
-        }
-        else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
+        }  
+      }
+      else if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
+        if (this.items[i].sellIn < 0) {
           this.items[i].quality = this.items[i].quality - this.items[i].quality;
-        } 
-        else if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        }
+      } 
+      else if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        if (this.items[i].sellIn < 0) {
           this.items[i].quality = this.decreaseQuality(this.items[i].quality)
         }
       }
-    }
-
+      }
     return this.items;
   }
 }
