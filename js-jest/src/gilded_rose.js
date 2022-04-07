02983b556ae0106adb9abc3bce8305f3371cd020
cindy.yeh <cindy.yeh@104.com.tw>
2022-04-07 15:16:07 +0800

    品質遞增/遞減功能抽出

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,23 +10,31 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
+
+  increaseQuality (quality) {
+    if( quality < 50 ) return quality + 1
+    return quality
+  }
+
+  decreaseQuality (quality){
+    if( quality > 0 ) return  quality - 1
+    return quality
+  }
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0 && this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
+        if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.decreaseQuality(this.items[i].quality);
         }
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+          this.items[i].quality = this.increaseQuality(this.items[i].quality)
           if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
+            if (this.items[i].sellIn < 11) {
+              this.items[i].quality = this.increaseQuality(this.items[i].quality)
             }
-            if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-              this.items[i].quality = this.items[i].quality + 1;
+            if (this.items[i].sellIn < 6) {
+              this.items[i].quality = this.increaseQuality(this.items[i].quality)
             }
-          }
         }
       }
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
